3cb5f51f7a50e502491c27d4cb2444ad34314bf8
pandachan123 <129178966+pandachan123@users.noreply.github.com>
2024-11-10 10:15:56 +0530

    Update client.c

--- Assignment_11/client.c
+++ Assignment_11/client.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-struct data { char str[100]; int flag; };
+#include <string.h>
+#include <unistd.h>
+struct Data {
+    char str[100];
+    int flag; // 0: ready to write, 1: data available, 2: client reading
+};
 int main() {
-    key_t key = ftok("abc.txt", 'A');
-    int shmid = shmget(key, sizeof(struct data), 0777 | IPC_CREAT);
-    struct data *d = (struct data *)shmat(shmid, NULL, 0);
-    while (d->flag != 1) { sleep(2); }
-    printf("Accepted data: %s", d->str);
-    d->flag = -1;
-    shmdt(d);
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, sizeof(struct Data), 0666 | IPC_CREAT);
+    struct Data* data = (struct Data*)shmat(shmid, (void*)0, 0);
+    while (1) {
+        while (data->flag != 1) {
+            printf("Client waiting...\n");
+            sleep(1); // Wait for the server to write
+        }
+        data->flag = 2; // Indicate that client is reading
+        printf("Client: Data read from memory: %s\n", data->str);
+        data->flag = 0; // Reset the flag to allow the server to write again
+        if (strcmp(data->str, "exit") == 0) break;
+    }
+    shmdt(data);
+    shmctl(shmid, IPC_RMID, NULL);
     return 0;
 }
