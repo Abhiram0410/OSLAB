38088eb4db4862fe5d287075d2b190f99c475565
pandachan123 <129178966+pandachan123@users.noreply.github.com>
2024-11-10 10:14:20 +0530

    Update client.c

--- Assignment_10/client.c
+++ Assignment_10/client.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 int main() {
-    const char *myfifo = "/tmp/myfifo";
-    char str2[80];
-    while (1) {
-        int fd = open(myfifo, O_WRONLY);
-        fgets(str2, sizeof(str2), stdin);
-        write(fd, str2, strlen(str2) + 1);
-        close(fd);
+    char *fifo1 = "fifo1";
+    char *fifo2 = "fifo2";
+    char buffer1[100];
+    int d1, d2, i = 0, cc = 0, ln = 0, wc = 0;
+    d1 = open(fifo1, O_RDONLY);
+    read(d1, buffer1, sizeof(buffer1));
+    while (buffer1[i] != '#' && buffer1[i] != '\0') {
+        if (buffer1[i] == ' ') {
+            wc++;
+        } else if (buffer1[i] == '\n') {
+            ln++;
+            wc++;
+        } else {
+            cc++;
+        }
+        i++;
     }
+    FILE *fp = fopen("Rajnandini.txt", "w");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    fprintf(fp, "\nLine count = %d", ln);
+    fprintf(fp, "\nWord count = %d", wc);
+    fprintf(fp, "\nCharacter count = %d", cc);
+    fclose(fp);
+    mkfifo(fifo2, 0666);
+    d2 = open(fifo2, O_WRONLY);
+    write(d2, "Rajnandini.txt", 14);
+    close(d2);
     return 0;
 }
